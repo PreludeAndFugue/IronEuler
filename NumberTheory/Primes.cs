8f6b470ef9dde63a74f091c530566f40da5bfc3c
Gary Kerr <gdrummondk@gmail.com>
2010-04-18 19:45:59 +0100

    Added PrimeFactor class.

--- NumberTheory/Primes.cs
+++ NumberTheory/Primes.cs
@@ -9,10 +9,34 @@ using System.Collections.Generic;
 
 namespace NumberTheory
 {
+	
+	/// <summary>
+	/// A class for holding some static methods to create lists of primes, etc.
+	/// </summary>
+	public class Primes
+	{
+		public static PrimesSieve CreatePrimesFromSieve(int n)
+		{
+			return new PrimesSieve(n);
+		}
+		
+		public static PrimeFactors GetPrimeFactors(int n, PrimesList primes)
+		{
+			return new PrimeFactors(n, primes);
+		}
+	}
+	
+	/// <summary>
+	/// A base class for the different classes the create a list of primes.
+	/// </summary>
+	public class PrimesList: List<int>
+	{
+	}
+	
 	/// <summary>
 	/// Generate all primes numbers less than or equal to max using a sieve.
 	/// </summary>
-	public class PrimesSieve : List<int>
+	public class PrimesSieve : PrimesList
 	{	
 		private int[] _primes;
 		
@@ -72,6 +96,49 @@ namespace NumberTheory
 				if (i !=0 ) {
 					Console.Out.WriteLine(i);
 				}
+			}
+		}
+	}
+	
+	/// <summary>
+	/// The prime factors of an integer n. A list of arrays of integers of length two
+	/// where the first integer is the prime factor, and the second prime is the
+	/// multiplicity. 
+	/// </summary>
+	public class PrimeFactors : List<int[]>
+	{
+		private int number;
+		
+		public PrimeFactors(int n, PrimesList primes)
+		{
+			number = n;
+			addFactors(primes);
+		}
+		
+		private void addFactors(PrimesList primes)
+		{
+			foreach (int p in primes)
+			{
+				addFactor(p);
+				if (number == 1)
+				{
+					break;
+				}
+			}
+		}
+		
+		private void addFactor(int p)
+		{
+			int multiplicity = 0;
+			while (number%p == 0 && number != 1)
+			{
+				multiplicity += 1;
+				number /= p;
+			}
+			if (multiplicity > 0)
+			{
+				int[] new_factor = {p, multiplicity};
+				this.Add(new_factor);
 			}
 		}
 	}
--- NumberTheoryTest/PrimesTest.cs
+++ NumberTheoryTest/PrimesTest.cs
@@ -21,27 +21,82 @@ namespace NumberTheoryTest
 				269,271,277,281,283,293,307,311,313,317,331,337,347,349,353,359,367,373,
 				379,383,389,397,401,409,419,421,431,433,439,443,449,457,461,463,467,479,
 				487,491,499,503,509,521,523,541};
-		PrimesSieve primes;
 		
 		[Test]
 		public void TestFirst100()
 		{
+			PrimesSieve primes = new PrimesSieve(541);
 			for (int i = 0; i < 100; i++)
 			{
 				Assert.AreEqual(small_primes[i], primes[i]);
 			}
 		}
 		
-		[TestFixtureSetUp]
+		[Test]
+		public void TestCreation()
+		{
+			PrimesSieve primes = Primes.CreatePrimesFromSieve(541);
+			for (int i = 0; i < 100; i++)
+			{
+				Assert.AreEqual(small_primes[i], primes[i]);
+			}
+		}
+	}
+	
+	[TestFixture]
+	public class PrimeFactorsTest
+	{
+		
+		PrimesList primes;
+		
+		[SetUp]
 		public void Init()
 		{
-			primes = new PrimesSieve(541);
+			primes = new PrimesSieve(10000);
 		}
 		
-		[TestFixtureTearDown]
-		public void Dispose()
+		[Test]
+		public void Test4()
 		{
-			// TODO: Add tear down code.
+			PrimeFactors pf = new PrimeFactors(4, primes);
+			int prime = pf[0][0];
+			int multiplicity = pf[0][1];
+			Assert.AreEqual(prime, 2);
+			Assert.AreEqual(multiplicity, 2);
+		}
+		
+		[Test]
+		public void Test2()
+		{
+			PrimeFactors pf = new PrimeFactors(2, primes);
+			int prime = pf[0][0];
+			int multiplicity = pf[0][1];
+			Assert.AreEqual(prime, 2);
+			Assert.AreEqual(multiplicity, 1);
+		}
+		
+		[Test]
+		public void Test6()
+		{
+			PrimeFactors pf = new PrimeFactors(6, primes);
+			int prime = pf[0][0];
+			int multiplicity = pf[0][1];
+			Assert.AreEqual(prime, 2);
+			Assert.AreEqual(multiplicity, 1);
+			prime = pf[1][0];
+			multiplicity = pf[1][1];
+			Assert.AreEqual(prime, 3);
+			Assert.AreEqual(multiplicity, 1);
+		}
+		
+		[Test]
+		public void Test23()
+		{
+			PrimeFactors pf = new PrimeFactors(23, primes);
+			int prime = pf[0][0];
+			int multiplicity = pf[0][1];
+			Assert.AreEqual(prime, 23);
+			Assert.AreEqual(multiplicity, 1);
 		}
 	}
 }
