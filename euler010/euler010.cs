4ec8c421c59425513e0ffb12758962d63ff688f3
Gary Kerr <gdrummondk@gmail.com>
2010-04-19 00:27:27 +0100

    Added euler010.

---
+++ euler010/euler010.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: gdk
+ * Date: 19/04/2010
+ * Time: 00:24
+ */
+using System;
+using NumberTheory;
+
+namespace euler010
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			long answer = 0;
+			PrimesList primes = new PrimesSieve(2000000);
+			foreach (int p in primes)
+			{
+				answer += p;
+			}
+			Console.Out.WriteLine("Answer: {0}", answer);
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
