5ed0f61ab7b0f69a42225c9cc6369568ee35d520
Gary Kerr <gdrummondk@gmail.com>
2010-04-19 23:22:07 +0100

    Added triangular numbers

---
+++ NumberTheory/Sequences.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: gdk
+ * Date: 19/04/2010
+ * Time: 23:00
+ */
+using System;
+
+namespace NumberTheory
+{
+	/// <summary>
+	/// Description of Sequences.
+	/// </summary>
+	public class Sequences
+	{
+		public Sequences()
+		{
+		}
+		
+		public static int TriangleNumber(int n)
+		{
+			if (n%2 == 0)
+			{
+				return (n/2)*(n + 1);
+			}
+			else
+			{
+				return ((n + 1)/2)*n;
+			}
+		}
+	}
+}
---
+++ NumberTheoryTest/SequencesTest.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: gdk
+ * Date: 19/04/2010
+ * Time: 23:06
+ */
+
+using System;
+using NUnit.Framework;
+using NumberTheory;
+
+namespace NumberTheoryTest
+{
+	[TestFixture]
+	public class SequencesTest
+	{
+		[Test]
+		public void TestTriangleNumbers()
+		{
+			int[,] pairs = {{0, 0}, {1, 1}, {2, 3}, {3, 6}, {4, 10},
+				{5, 15}, {6, 21}, {7, 28}, {8, 36}, {9, 45}, {10, 55}};
+			for (int i = 0; i < pairs.Length/2; i ++)
+			{
+				Assert.AreEqual(Sequences.TriangleNumber(pairs[i, 0]), pairs[i, 1]);
+			}
+		}
+	}
+}
