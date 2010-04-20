9ada816dfcb74209a83e860b59d815eb99a2a1c2
PreludeAndFugue <gdrummondk@gmail.com>
2010-04-20 13:38:15 +0100

    Added Sequence.hailstone and tests.

--- NumberTheoryTest/SequencesTest.cs
+++ NumberTheoryTest/SequencesTest.cs
@@ -24,5 +24,24 @@ namespace NumberTheoryTest
 				Assert.AreEqual(Sequences.TriangleNumber(pairs[i, 0]), pairs[i, 1]);
 			}
 		}
+		
+		[Test]
+		public void TestHailstone()
+		{
+		    int[] hail10 = {10, 5, 16, 8, 4, 2, 1};
+		    int[] hail2 = {2, 1};
+		    int[] hail21 = {21, 64, 32, 16, 8, 4, 2, 1};
+		    int[] hail7 = {7, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1};
+		    int[][] tests = {hail10, hail2, hail21, hail7};
+		    foreach (int[] test in tests)
+		    {
+		        int[] hail = Sequences.Hailstone(test[0]);
+		        Assert.AreEqual(hail.Length, test.Length);
+		        for (int i = 0; i < hail.Length; i++)
+		        {
+		            Assert.AreEqual(hail[i], test[i]);
+		        }
+		    }
+		}
 	}
 }
