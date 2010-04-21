83b4ddd864a2ba8902f47c7e45f4cfb3eebea898
PreludeAndFugue <gdrummondk@gmail.com>
2010-04-21 11:24:58 +0100

    Completed euler016.

--- euler016/euler016.cs
+++ euler016/euler016.cs
@@ -12,6 +12,7 @@
  */
  
 using System;
+using System.Collections.Generic;
 
 namespace euler016
 {
@@ -19,12 +20,60 @@ namespace euler016
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> number = new List<int>();
+            number.Add(1);
+            for (int i = 0; i < 1000; i++)
+            {
+                number = LongMultiplication(number, 2);
+                //Console.Out.WriteLine(ListToString(number));
+            }
             
-            // TODO: Implement Functionality Here
+            number.Reverse();
+            Console.Out.WriteLine(ListToString(number));
+            Console.Out.WriteLine("Answer: {0}", sumDigits(number));
             
             Console.Write("Press any key to continue . . . ");
             Console.ReadKey(true);
+        }
+        
+        public static List<int> LongMultiplication(List<int> number, int multiple)
+        {
+            int tens = 0;
+            List<int> answer = new List<int>();
+            foreach (int n in number)
+            {
+                int new_n = n*multiple + tens;
+                tens = new_n/10;
+                int new_n_unit = new_n%10;
+                answer.Add(new_n_unit);
+            }
+            while (tens != 0)
+            {
+                answer.Add(tens%10);
+                tens /= 10;
+            }
+            //answer.Reverse();
+            return answer;
+        }
+        
+        public static string ListToString(List<int> number)
+        {
+            string answer = "";
+            foreach (int n in number)
+            {
+                answer += n.ToString();
+            }
+            return answer;
+        }
+        
+        public static int sumDigits(List<int> number)
+        {
+            int answer = 0;
+            foreach (int i in number)
+            {
+                answer += i;
+            }
+            return answer;
         }
     }
 }
