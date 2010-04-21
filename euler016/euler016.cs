c849413f523a23af97c6ab06f374b094a1f2c18d
PreludeAndFugue <gdrummondk@gmail.com>
2010-04-21 12:26:01 +0100

    Second version of euler016 created in same file.

--- euler016/euler016.cs
+++ euler016/euler016.cs
@@ -20,6 +20,22 @@ namespace euler016
     {
         public static void Main(string[] args)
         {
+            ListNumber n = new ListNumber(1);
+            for (int i = 0; i < 1000; i++)
+            {
+                n = n*2;
+                //Console.Out.WriteLine(n);
+            }
+            
+            Console.Out.WriteLine(n);
+            Console.Out.WriteLine("Answer: {0}", n.DigitSum());
+            
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+        
+        public static void Main1(string[] args)
+        {
             List<int> number = new List<int>();
             number.Add(1);
             for (int i = 0; i < 1000; i++)
@@ -29,7 +45,8 @@ namespace euler016
             }
             
             number.Reverse();
-            Console.Out.WriteLine(ListToString(number));
+            Console.Out.WriteLine(number.ToString());
+            //Console.Out.WriteLine(ListToString(number));
             Console.Out.WriteLine("Answer: {0}", sumDigits(number));
             
             Console.Write("Press any key to continue . . . ");
@@ -72,6 +89,67 @@ namespace euler016
             foreach (int i in number)
             {
                 answer += i;
+            }
+            return answer;
+        }
+    }
+    
+    class ListNumber : List<int>
+    {
+        public ListNumber()
+        {}
+        
+        public ListNumber(int n)
+        {
+            while (n != 0)
+            {
+                this.Add(n%10);
+                n /= 10;
+            }
+        }
+        
+        public ListNumber(List<int> number)
+        {
+            this.AddRange(number);
+        }
+        
+        public static ListNumber operator *(ListNumber number, int multiple)
+        {
+            int tens = 0;
+            ListNumber answer = new ListNumber();
+            foreach (int n in number)
+            {
+                int new_n = n*multiple + tens;
+                tens = new_n/10;
+                int new_n_unit = new_n%10;
+                answer.Add(new_n_unit);
+            }
+            while (tens != 0)
+            {
+                answer.Add(tens%10);
+                tens /= 10;
+            }
+            //answer.Reverse();
+            return answer;
+        }
+        
+        public int DigitSum()
+        {
+            int answer = 0;
+            foreach (int i in this)
+            {
+                answer += i;
+            }
+            return answer;
+        }
+        
+        public override string ToString()
+        {
+            string answer = "";
+            
+            foreach (int i in this)
+            {
+                answer = i.ToString() + answer;
             }
             return answer;
         }
